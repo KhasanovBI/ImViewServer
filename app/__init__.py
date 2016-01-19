ab49f67e8fcb2294b5a1b20670f58a9a45347ec7
Bulat Khasanov <afti@yandex.ru>
2016-01-19 04:40:54 +0300

    remove logging

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-import logging
-from logging.handlers import RotatingFileHandler
 
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
-
-handler = RotatingFileHandler('foo.log', maxBytes=10000, backupCount=1)
-handler.setLevel(logging.INFO)
-app.logger.addHandler(handler)
 
 from app import views, models
